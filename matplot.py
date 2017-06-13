f983c75c0f7e19b7535cdb7537842de446bd6557
zhangchenghao <zhangchenghao@hfbank.com.cn>
2017-06-13 09:20:43 +0800

    add matplot

---
+++ matplot.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+np.random.seed(1000)
+y = np.random.standard_normal(20)
+
+x = range(len(y))
+print x
+plt.plot(x,y)
+plt.show()
+
