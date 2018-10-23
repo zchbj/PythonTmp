a8b3a9608a9dbce637e861ed312f2138fe3380b8
zhangchenghao <zhangchenghao@hfbank.com.cn>
2018-10-23 22:56:20 +0800

    optimize function

--- box/box.py
+++ box/box.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 import math
 
+
 def getArray(list, maxLen, paddingLeft):
-    resultArray = [] #得到组合结果
     nextList = [] #多余的数列，需要进入迭代求解
     subResultArray = [] #从迭代计算中获取到的组合结果
     list.sort(reverse = True) #数列倒排序
@@ -11,16 +11,17 @@ def getArray(list, maxLen, paddingLeft):
     resultArray = [[] for n in range(rowNum)]
     rowIndex = 0 #第一行开始插入
     nextValueIndex = 0 # 第一次默认从第一个位置开始
-    while (nextValueIndex) > -1:
+    while (nextValueIndex) < len(list):
         # 把获取到符合条件的值插入到最短行的队尾
         resultArray[rowIndex].append(list[nextValueIndex])
 
         # 如果取值有跳跃，把跳跃过去的值放入迭代计算列表
         if nextValueIndex > 0:
-            nextList.append(list[0:nextValueIndex - 1])
+            nextList.append(list[0:nextValueIndex + 1])
         # 截取list，进入下一轮
-        list = list[nextValueIndex + 1:]
+        del list[0:nextValueIndex + 1]
         rowIndex = getSumMinRowIndex(resultArray)
+        # 可用长度=最大长度-已用长度-数字个数*间隔长度
         nextMaxValue = maxLen - sum(resultArray[rowIndex]) - len(resultArray[rowIndex]) * paddingLeft
         nextValueIndex = getFirstValueIndex(list, nextMaxValue)
 
@@ -35,35 +36,30 @@ def getArray(list, maxLen, paddingLeft):
 
     return resultArray
 
+
 #返回列表中小于目标值的第一数字的位置
 def getFirstValueIndex(list, maxValue):
-    firstValueIndex = -1
-    for index in range(len(list)):
-        if list[index] <= maxValue:
-            firstValueIndex = index
-            break
-    return firstValueIndex
+    return sum([1 if i > maxValue else 0 for i in list])
 
 #返回行数组中，行数字和最小的数组编号
 def getSumMinRowIndex(rowList):
-    rowSumList = []
-    for index in range(len(rowList)):
-        rowSumList.append((index, sum(rowList[index])))
-    rowSumList.sort(key=lambda x:x[1]) #根据第二个字段(行中数字之和）排序
-    return  rowSumList[0][0]
+    rowSumList = [sum(subList) for subList in rowList]
+    return rowSumList.index(min(rowSumList))
+
 
 # 数列进行行组合处理过程
 def doList(industryLenList):
     columnMaxLen = 30
     paddingLeft =2
     industryLenList.sort(reverse=True)
-    if max(industryLenList) > columnMaxLen: print "单个长度超过行长度限制"
-    result = getArray(industryLenList, columnMaxLen, paddingLeft)
-    result.sort(key=lambda x:sum(x)+len(x)*2, reverse=True) # 根据结果的行的长度倒排序
     rowLeastCount = math.ceil((sum(industryLenList) + len(industryLenList)*paddingLeft)* 1.0/(columnMaxLen+paddingLeft))
     print '排序后列表：'
     print industryLenList
     print '列表之和：', sum(industryLenList), '; 行长度限制：', columnMaxLen, '; 间距：', paddingLeft, '; 预计至少需要', rowLeastCount, '行'
+
+    if max(industryLenList) > columnMaxLen: print "单个长度超过行长度限制"
+    result = getArray(industryLenList, columnMaxLen, paddingLeft)
+    result.sort(key=lambda x:sum(x)+len(x)*2, reverse=True) # 根据结果的行的长度倒排序
     print '结果如下:'
     print result
     print '123456789012345678901234567890'
@@ -73,6 +69,7 @@ def doList(industryLenList):
             x = x + "X" * result[rowIndex][colIndex] + "  "
         print x
     return
+
 
 # 主函数
 def main():
