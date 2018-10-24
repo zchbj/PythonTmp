1fecb31a8275116017846b0c1987cf32cd366ddd
zhangchenghao <zhangchenghao@hfbank.com.cn>
2018-10-24 22:35:42 +0800

    fix some bug and modify code style

--- box/box.py
+++ box/box.py
@@ -3,87 +3,77 @@
 import math
 
 
-def getArray(list, maxLen, paddingLeft):
-    nextList = [] #多余的数列，需要进入迭代求解
-    subResultArray = [] #从迭代计算中获取到的组合结果
-    list.sort(reverse = True) #数列倒排序
-    rowNum = int(math.ceil((sum(list) + len(list)*paddingLeft )*1.0 / (maxLen+paddingLeft))) #至少需要N+1行,N= 总和/行长度
-    resultArray = [[] for n in range(rowNum)]
-    rowIndex = 0 #第一行开始插入
-    nextValueIndex = 0 # 第一次默认从第一个位置开始
-    while (nextValueIndex) < len(list):
-        # 把获取到符合条件的值插入到最短行的队尾
-        resultArray[rowIndex].append(list[nextValueIndex])
+def get_array(industries, max_len, padding_left):
+    industries.sort(reverse=True)  # 数列倒排序
+    # 至少需要N+1行,N= 总和/行长度
+    row_num = int(math.ceil((sum(industries) + len(industries)*padding_left)*1.0 / (max_len + padding_left)))
+    rows = [[] for n in range(row_num)]
+    row_index = 0  # 第一行开始插入
+    next_value_index = 0  # 第一次默认从第一个位置开始
+    while next_value_index < len(industries):
+        rows[row_index].append(industries[next_value_index])  # 把获取到符合条件的值插入到最短行的队尾
+        del industries[next_value_index]  # 待处理队列中删除符合条件的值
 
-        # 如果取值有跳跃，把跳跃过去的值放入迭代计算列表
-        if nextValueIndex > 0:
-            nextList.append(list[0:nextValueIndex + 1])
-        # 截取list，进入下一轮
-        del list[0:nextValueIndex + 1]
-        rowIndex = getSumMinRowIndex(resultArray)
+        # 进入下一轮
+        row_index = get_sum_min_row_index(rows, padding_left)
         # 可用长度=最大长度-已用长度-数字个数*间隔长度
-        nextMaxValue = maxLen - sum(resultArray[rowIndex]) - len(resultArray[rowIndex]) * paddingLeft
-        nextValueIndex = getFirstValueIndex(list, nextMaxValue)
+        next_max_value = max_len - sum(rows[row_index]) - len(rows[row_index])*padding_left
+        next_value_index = get_first_value_index(industries, next_max_value)
 
-    nextList.extend(list) # 没处理完的列表也放入到下次迭代中去
-    if len(nextList) > 0:
+    if len(industries) > 0:
         # 多余的数列不为空，递归调用求解
-        subResultArray = getArray(nextList, maxLen, paddingLeft)
-
-    #返回本次解和迭代解的合集
-    if len(subResultArray)>0:
-        resultArray.extend(subResultArray)
-
-    return resultArray
+        sub_rows = get_array(industries, max_len, padding_left)
+        if len(sub_rows) > 0:
+            rows.extend(sub_rows)
+    return rows
 
 
-#返回列表中小于目标值的第一数字的位置
-def getFirstValueIndex(list, maxValue):
-    return sum([1 if i > maxValue else 0 for i in list])
+# 返回列表中小于目标值的第一数字的位置
+def get_first_value_index(industries, max_value):
+    return sum([1 if i > max_value else 0 for i in industries])
 
-#返回行数组中，行数字和最小的数组编号
-def getSumMinRowIndex(rowList):
-    rowSumList = [sum(subList) for subList in rowList]
-    return rowSumList.index(min(rowSumList))
+
+# 返回行数组中，行数字和最小的数组编号
+def get_sum_min_row_index(rows, padding_left):
+    rows_sum = [sum(nums) + len(nums)*padding_left for nums in rows]
+    return rows_sum.index(min(rows_sum))
 
 
 # 数列进行行组合处理过程
-def doList(industryLenList):
-    columnMaxLen = 30
-    paddingLeft =2
-    industryLenList.sort(reverse=True)
-    rowLeastCount = math.ceil((sum(industryLenList) + len(industryLenList)*paddingLeft)* 1.0/(columnMaxLen+paddingLeft))
+def magic(industries, column_max_len=30, padding_left=2):
+    if max(industries) > column_max_len:
+        print "单个长度超过行长度限制"
+        return
+    industries.sort(reverse=True)
     print '排序后列表：'
-    print industryLenList
-    print '列表之和：', sum(industryLenList), '; 行长度限制：', columnMaxLen, '; 间距：', paddingLeft, '; 预计至少需要', rowLeastCount, '行'
-
-    if max(industryLenList) > columnMaxLen: print "单个长度超过行长度限制"
-    result = getArray(industryLenList, columnMaxLen, paddingLeft)
-    result.sort(key=lambda x:sum(x)+len(x)*2, reverse=True) # 根据结果的行的长度倒排序
+    print industries
+    print '列表之和：', sum(industries), '; 行长度限制：', column_max_len, '; 间距：', padding_left
+    result = get_array(industries, column_max_len, padding_left)
+    result.sort(key=lambda a: sum(a)+len(a)*2, reverse=True)  # 根据结果的行的长度倒排序
     print '结果如下:'
     print result
-    print '123456789012345678901234567890'
+    print '1234567890'*8
     for rowIndex in range(len(result)):
         x = ''
         for colIndex in range(len(result[rowIndex])):
-            x = x + "X" * result[rowIndex][colIndex] + "  "
+            x = x + "X"*result[rowIndex][colIndex] + "  "
         print x
     return
 
 
 # 主函数
 def main():
-    industryLenList = [9, 3, 3, 16, 3, 6, 11, 6, 15, 3, 4, 8, 10, 13, 13, 2, 7, 9, 14, 4]
-    doList(industryLenList)
-    industryLenList = [16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16]
-    doList(industryLenList)
-    industryLenList = [16, 16, 16, 16, 16, 16, 13, 13, 13, 13, 13, 13]
-    doList(industryLenList)
-    industryLenList = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-    doList(industryLenList)
-    industryLenList = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
-    doList(industryLenList)
-    industryLenList = [2, 18, 4, 8, 12, 23, 13, 1, 2, 4, 6, 7, 8, 9, 10, 2, 2, 18, 4, 8, 12, 23, 13, 1, 2, 4, 6, 7, 8, 9, 10, 2]
-    doList(industryLenList)
+    industries = [9, 3, 3, 16, 3, 6, 11, 6, 15, 3, 4, 8, 10, 13, 13, 2, 7, 9, 14, 4]
+    magic(industries)
+    industries = [16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16, 16]
+    magic(industries)
+    industries = [16, 16, 16, 16, 16, 16, 13, 13, 13, 13, 13, 13]
+    magic(industries)
+    industries = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    magic(industries)
+    industries = [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    magic(industries)
+    industries = [2, 18, 4, 8, 12, 23, 13, 1, 2, 4, 6, 7, 8, 9, 10, 2, 2, 18, 4, 8, 12, 23, 13, 1, 2, 4, 6, 7, 8, 9]
+    magic(industries)
 
 main()
